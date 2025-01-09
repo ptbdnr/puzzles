cb233b4a4b66cfa5ac0c3ae80f018609fb95fe36
Peter Bodnar <ptbdnr@gmai.com>
2025-01-09 23:15:11 +0000

    feat: img

--- python/algos/pascal_triangles.py
+++ python/algos/pascal_triangles.py
@@ -1,6 +1,13 @@
 """Given an integer numRows, return the first numRows of Pascal's triangle.
 
 In Pascal's triangle, each number is the sum of the two numbers directly above it.
+```
+    1
+   1 1
+  1 2 1
+ 1 3 3 1
+1 4 6 4 1
+```
 """
 
 class Solution:
