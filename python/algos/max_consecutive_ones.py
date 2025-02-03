62d756cd958aa118e9f85bea253b9327f37ae5dd
Peter <32688266+ptbdnr@users.noreply.github.com>
2025-02-03 22:34:52 +0000

    fix: 485

--- python/algos/max_consecutive_ones.py
+++ python/algos/max_consecutive_ones.py
@@ -1,16 +1,14 @@
 """Given a binary array nums, return the maximum number of consecutive 1's in the array."""
 class Solution:
     def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
-        n =len(nums)
-        out =0
-        count =0
-        for i in range(n):
-            if nums[i] == 1:
-                count+=1
+        curr, out = 0, 0
+        for num in nums:
+            if num == 1:
+                curr += 1
             else:
-                if count > out:
-                    out =count
-                count=0
-        if count > out:
-            return count    
+                if curr > out:
+                    out = curr
+                curr = 0
+        if curr > out:
+            return curr
         return out
